a99083d52502a785eb3da4451cf3d232b6a3717d
Steve Spicklemire <Steve Spicklemire>
2014-07-26 22:00:00 -0400

    copy models and initdb code

--- webapp/assessdb/models.py
+++ webapp/assessdb/models.py
@@ -3,6 +3,9 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    Binary,
+    DateTime,
+    ForeignKey,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,18 +13,123 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
+    relationship,
+    backref,
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
+import datetime
+
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-
-class MyModel(Base):
-    __tablename__ = 'models'
+class Courses(Base):
+    __tablename__ = 'courses'
+    id = Column(Integer, primary_key=True)
+    subject = Column(Text)
+    num = Column(Integer)
+    sect = Column(Text)
+    term = Column(Integer)
+    CRN = Column(Integer)
+    
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    first = Column(Text)
+    last = Column(Text)
+    upid = Column(Text)
+    
+class Roles(Base):
+    __tablename__ = 'roles'
     id = Column(Integer, primary_key=True)
     name = Column(Text)
-    value = Column(Integer)
+    
+class CoursesPeople(Base):
+    """
+    Association between People and Courses
+    """
+    __tablename__ = 'courses_people'
+    courseid = Column(Integer, ForeignKey('courses.id'), primary_key=True)
+    personid = Column(Integer, ForeignKey('people.id'), primary_key=True)
+    roleid = Column(Integer, ForeignKey('roles.id'), primary_key=True)
+    person = relationship("People", backref="courses_assocs")
+    course = relationship("Courses", backref="people_assocs")
+    role = relationship("Roles")
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+class Instruments(Base):
+    __tablename__ = 'instruments'
+    id = Column(Integer, primary_key=True)
+    ancestorid = Column(Integer, ForeignKey('instruments.id'))
+    name = Column(Text)
+    description = Column(Text)
+    genetics = Column(Text)
+    
+class Objectives(Base):
+    __tablename__ = 'objectives'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    description = Column(Text)
+
+class Assessments(Base):
+    __tablename__ = 'assessments'
+    id = Column(Integer, primary_key=True)
+    type = Column(Text)
+    courseid = Column(Integer, ForeignKey('courses.id'))
+    instrumentid = Column(Integer, ForeignKey('instruments.id'))
+    tstamp = Column(DateTime, default=datetime.datetime.utcnow)
+    
+class Responses(Base):
+    __tablename__ = 'responses'
+    id = Column(Integer, primary_key=True)
+    assessmentid = Column(Integer, ForeignKey('assessments.id'))
+    personid = Column(Integer, ForeignKey('people.id'))
+    itemid = Column(Integer, ForeignKey('items.id'))
+    value = Column(Text)
+    tstamp = Column(DateTime, default=datetime.datetime.utcnow)
+    
+class Items(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    ancestorid = Column(Integer, ForeignKey('items.id'), default=None)
+    markup = Column(Text)
+    genetics = Column(Text, default='')
+
+class InstrumentItems(Base):
+    __tablename__ = 'instrument_items'
+    instrumentid = Column(Integer, ForeignKey('instruments.id'), primary_key=True)
+    itemid = Column(Integer, ForeignKey('items.id'), primary_key=True)
+    contextid = Column(Text)
+    
+class Blobs(Base):
+    __tablename__ = 'blobs'
+    id = Column(Integer, primary_key=True)
+    type = Column(Text)
+    data = Column(Binary)
+
+class Answers(Base):
+    __tablename__ = 'answers'
+    id = Column(Integer, primary_key=True)
+    markup = Column(Text)
+    
+class AnswerBlobs(Base):
+    __tablename__ = 'answer_blobs'
+    answerid = Column(Integer, ForeignKey('answers.id'), primary_key=True)
+    blobid = Column(Integer, ForeignKey('blobs.id'), primary_key=True)
+    
+class ItemBlobs(Base):
+    __tablename__ = 'item_blobs'
+    id = Column(Integer, primary_key=True)
+    itemid = Column(Integer, ForeignKey('items.id'))
+    blobid = Column(Integer, ForeignKey('blobs.id'))
+   
+class ItemAnswers(Base):
+    __tablename__ = 'item_answers'
+    itemid = Column(Integer, ForeignKey('items.id'), primary_key=True)
+    answerid = Column(Integer, ForeignKey('answers.id'), primary_key=True)
+    answerattr = Column(Text, default='')
+    
+class ItemObjectives(Base):
+    __tablename__ = 'itemo_bjectives'
+    itemid = Column(Integer, ForeignKey('items.id'), primary_key=True)
+    objectiveid = Column(Integer, ForeignKey('objectives.id'), primary_key=True)
--- webapp/assessdb/scripts/initializedb.py
+++ webapp/assessdb/scripts/initializedb.py
@@ -13,17 +13,30 @@ from pyramid.scripts.common import parse_vars
 
 from ..models import (
     DBSession,
-    MyModel,
+    Courses,
+    People,
+    Roles,
+    Instruments,
+    CoursesPeople,
+    Assessments,
+    Responses,
+    Answers,
+    Items,
+    ItemAnswers,
+    Objectives,
+    ItemObjectives,
+    InstrumentItems,
+    Blobs,
+    ItemBlobs,
+    AnswerBlobs,
     Base,
     )
-
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri> [var=value]\n'
           '(example: "%s development.ini")' % (cmd, cmd))
     sys.exit(1)
-
 
 def main(argv=sys.argv):
     if len(argv) < 2:
@@ -36,5 +49,40 @@ def main(argv=sys.argv):
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
     with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+        instr_role = Roles(name='instructor')
+        DBSession.add(instr_role)
+        stud_role = Roles(name='student')
+        DBSession.add(stud_role)
+        instr = People(first='Joe',last='Smith',upid='A101')
+        DBSession.add(instr)
+        stud = People(first='Sarah',last='Jones',upid='A099')
+        DBSession.add(stud)
+        phys = Courses(subject='PHYS',num=150, sect=1, term=132, CRN=12345)
+        DBSession.add(phys)
+        DBSession.flush()
+        assoc1 = CoursesPeople(courseid=phys.id, personid=instr.id, roleid=instr_role.id)
+        DBSession.add(assoc1)
+        assoc2 = CoursesPeople(courseid=phys.id, personid=stud.id, roleid=stud_role.id)
+        DBSession.add(assoc2)
+        fmci = Instruments(name='FMCI', description="Force Motion Concept Inventory")
+        DBSession.add(fmci)
+        kine = Objectives(name='Kinematics', description='position, velocity, acceleration and all that')
+        DBSession.add(kine)
+        DBSession.flush()
+        item1 = Items(markup="Question 1")
+        DBSession.add(item1)
+        DBSession.flush()
+        assoc3 = ItemObjectives(itemid=item1.id, objectiveid=kine.id)
+        DBSession.add(assoc3)
+        assoc4 = InstrumentItems(instrumentid=instr.id, itemid=item1.id, contextid='1')
+        DBSession.add(assoc4)
+        ans1 = Answers(markup="Answer 1")
+        DBSession.add(ans1)
+        DBSession.flush()
+        itm_ans_assoc1 = ItemAnswers(itemid=item1.id, answerid=ans1.id, answerattr="C")
+        DBSession.add(itm_ans_assoc1)
+        assess1 = Assessments(type='pre',courseid=phys.id, instrumentid=fmci.id)
+        DBSession.add(assess1)
+        DBSession.flush()
+        resp1 = Responses(assessmentid=assess1.id, itemid=item1.id, personid=stud.id, value='A')
+        DBSession.add(instr_role)
