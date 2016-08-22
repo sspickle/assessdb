61dd8dc3e43e45506c82b3cb66132cd745245fa4
Steve Spicklemire <steve@spvi.com>
2016-08-22 05:50:49 -0400

    clear out old requirejs stuff

--- webapp/assessdb/app/scripts/entities/entitiesObject.js
+++
@@ -1,8 +0,0 @@
-define(["backbone", "backbone.marionette"],
-    function (Backbone) {
-
-    var Entities = Backbone.Marionette.Object.extend({});
-    
-    return Entities;
-});
-
--- webapp/assessdb/app/scripts/views/StaticView.js
+++
@@ -1,7 +0,0 @@
-define(['backbone', 'backbone.marionette','bootstrap'],
-    function (Backbone) {
-        //ItemView provides some default rendering logic
-        return Backbone.Marionette.ItemView.extend({
-            template:'<div> This is a Contact Page: Not done</div>'
-        });
-    });
